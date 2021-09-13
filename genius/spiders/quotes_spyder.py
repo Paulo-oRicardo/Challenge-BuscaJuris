fca3e804fe8537f41659bc5612898bce398bcea1
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-09-13 19:47:52 -0300

    Cria arquivo quotes.json

---
+++ genius/spiders/quotes_spyder.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com/page/3/',
+        'http://quotes.toscrape.com/page/4/',
+        'http://quotes.toscrape.com/page/5/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'quote': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
